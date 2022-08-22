a41483caf8e3ce82b36ce65dea6b7d6445eef218
dresan22 <andresat48@hotmail.com>
2022-08-22 01:29:18 -0400

    fix open modal

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -83,14 +83,14 @@ export function Table() {
                     {list?.filter(pokemon => {
                         return fieldsFilter?.some((key => String(pokemon[key]).toLowerCase().includes(query)))
                     }).map(pokemon => {
-                        return (<tr key={pokemon.id} onClick={handlePokemonModal.bind(pokemon)} className='table-pokemon-title'>
+                        return (<tr key={pokemon.id} className='table-pokemon-title'>
                             {editPokemonId !== pokemon.id ? (
                                 <>
-                                    <td >
+                                    <td onClick={handlePokemonModal.bind(pokemon)}>
                                         <span>{pokemon.name}</span></td>
-                                    <td><img src={pokemon.image} alt={pokemon.name} className="pokemon-image" />  </td>
-                                    <td>{pokemon.attack}</td>
-                                    <td>{pokemon.defense}</td>
+                                    <td onClick={handlePokemonModal.bind(pokemon)}><img src={pokemon.image} alt={pokemon.name} className="pokemon-image" />  </td>
+                                    <td onClick={handlePokemonModal.bind(pokemon)}>{pokemon.attack}</td>
+                                    <td onClick={handlePokemonModal.bind(pokemon)}>{pokemon.defense}</td>
                                     <td><AiFillEdit onClick={e => handleEditClick(e, pokemon)} /> <AiFillDelete onClick={handleDeleteClick.bind(pokemon)} /></td>
                                 </>
                             ) : (<EditableRow
